ddc90dd535c4188450e6b32e336c6df8ea481b59
yasha <yasha@LENOVOV15>
2023-07-11 22:02:49 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿static int FindGreatestCommonDivison(int firstNumber, int secondNumber)
+{
+    if (secondNumber != 0)
+        return FindGreatestCommonDivison(secondNumber, firstNumber % secondNumber);
+    else
+        return firstNumber;
+}
+
+Console.Write("Write First Number: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write("Write Second Number: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+int greatCommonDivision = FindGreatestCommonDivison(firstNumber, secondNumber);
+Console.Write($"Greatest Common Divisor of {firstNumber} and {secondNumber} is {greatCommonDivision}.");
+
